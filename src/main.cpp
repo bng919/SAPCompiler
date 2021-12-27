dc022a20265c8ccc61ca4e2f6725412fa5c29254
bng919 <ben.n.graham@icloud.com>
2021-12-27 01:47:25 -0700

    Updated arg parsing using cxxopts. Options now include input, output, base, display, verbose, help

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
  *      Author: benng
  */
 
-//#include "compiler.h"
+#include "compiler.h"
 #include "cxxopts.hpp"
 
 
@@ -15,21 +15,28 @@ int main(int argc, char* argv[]){
 	cxxopts::Options options("sapcmp", "Simple CLI to compile a custom assembly language for a Ben Eater inspired 8-bit SAP (Simple As Possible) CPU.");
 
 	options.add_options()
-		("o,output", "Output file", cxxopts::value<std::string>())
+		("i,input", "Input file", cxxopts::value<std::string>())
+		("o,output", "Output file", cxxopts::value<std::string>()->default_value("sapcmp_output.txt"))
 		("b,base", "Infile argument base", cxxopts::value<int>()->default_value("2"))
-		("d,display", "Display conversion", cxxopts::value<bool>()->default_value("true"))
+		("d,display", "Display conversion", cxxopts::value<bool>()->implicit_value("true"))
 		("v,verbose", "Verbose (0,1)", cxxopts::value<int>()->default_value("0"))
 		("h,help", "Print usage")
 	;
 
 	auto results = options.parse(argc, argv);
 
-	std::string outfile;
-		if(results.count("output"))
-			outfile = results["output"].as<std::string>();
+	std::string infile;
+	if(results.count("input"))
+		infile = results["input"].as<std::string>();
+
+	std::string outfile = results["output"].as<std::string>();
 
 	int base = results["base"].as<int>();
-	bool display = results["display"].as<bool>();
+
+	bool display = false;
+	if(results.count("display"))
+		display = results["display"].as<bool>();
+
 	int verbose = results["verbose"].as<int>();
 
 	if(results.count("help")) {
@@ -37,40 +44,13 @@ int main(int argc, char* argv[]){
 	  exit(0);
 	}
 
-
-
-
-
-	/*
-	std::cout << "You have entered " << argc
-		 << " arguments:" << "\n";
-
-	for (int i = 0; i < argc; ++i)
-		std::cout << argv[i] << "\n";
-
-
-	std::cout << "starting";
-
-
-
-	std::vector<std::string> inVec = readFile(argv[1]);
-
-	//std::cout << inVec[0] << std::endl;
-
-
-
-	Program prog;
-
-	for(std::string& cmd : inVec) {
-		//std::cout << cmd;
-		Command tmp(cmd, 2);
-		prog.addCommand(tmp);
+	if(!results.count("input")) {
+		//TODO: Display conversion table
+		std::cout << "No input or output file";
+		exit(1);
 	}
 
-	prog.printConversion();
-
-*/
-
+	compile(infile, outfile, base, display, verbose);
 
 	return 0;
 }
